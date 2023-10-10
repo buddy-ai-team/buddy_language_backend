9f0296412582cc6dbf2d3dee824d5e0526ffb801
IppolitovIS <IppolitovIS@gmail.com>
2023-10-11 00:06:39 +0300

    Initial commint

---
+++ backend/infrastructure/BuddyLanguage.Data.EntityFramework/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BuddyLanguage.Data.EntityFramework;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(
+        DbContextOptions<AppDbContext> options) :
+        base(options)
+    {
+    }
+}
---
+++ tests/BuddyLanguage.Domain.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace BuddyLanguage.Domain.Test;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.Fail("No tests written");
+    }
+}
