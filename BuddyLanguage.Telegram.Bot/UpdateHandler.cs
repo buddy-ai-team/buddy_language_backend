5afdb737bfb10f4fcec0746344c80915b64379e5
Ekaterina-timans <128321173+Ekaterina-timans@users.noreply.github.com>
2023-10-23 17:07:17 +0300

    Добавлены методы получения и отправки голосовых сообщений

--- BuddyLanguage.Telegram.Bot/UpdateHandler.cs
+++ BuddyLanguage.Telegram.Bot/UpdateHandler.cs
@@ -38,24 +38,38 @@ namespace BuddyLanguage.Telegram.Bot
 		{
 			_logger.LogInformation("Receive message type: {MessageType}", message.Type);
 			if (message.Text is not { } messageText)
+			{
+				if (message.Voice != null)
+				{
+					await ReceiveVoiceMessage(_botClient, message, cancellationToken);
+				}
 				return;
+			}
 
 			var action = messageText.Split(' ')[0] switch
 			{
-				"/photo" => SendFile(_botClient, message, cancellationToken), // Отправляет пользователю изображение (фотографию) в сообщении
-				"/inline_mode" => StartInlineQuery(_botClient, message, cancellationToken), // Пользователь может нажать кнопку для начала взаимодействия с ботом
-
+				"/start" => StartCommand(_botClient, message, cancellationToken),
 				"/menu" => Usage(_botClient, message, cancellationToken) // Отправляет сообщение с инструкциями по использованию бота
 			};
 			Message sentMessage = await action;
 			_logger.LogInformation("The message was sent with id: {SentMessageId}", sentMessage.MessageId);
 		}
+		static async Task<Message> StartCommand(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+		{
+			const string welcomeMessage = "Привет! Я ваш бот. Чем могу помочь вам сегодня?\n" +
+										  "Вот список доступных команд:\n" +
+										  "/menu - показать это меню";
 
+			return await botClient.SendTextMessageAsync(
+				chatId: message.Chat.Id,
+				text: welcomeMessage,
+				replyMarkup: new ReplyKeyboardRemove(),
+				cancellationToken: cancellationToken);
+		}
 		static async Task<Message> Usage(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
 		{
 			const string usage = "Usage:\n" +
-								 "/photo       - send a photo\n" +
-								 "/inline_mode - send keyboard with Inline Query";
+								 "/inline_mode";
 
 			return await botClient.SendTextMessageAsync(
 				chatId: message.Chat.Id,
@@ -64,45 +78,30 @@ namespace BuddyLanguage.Telegram.Bot
 				cancellationToken: cancellationToken);
 		}
 
-		// Отправляет пользователю изображение(фотографию) в сообщении
-		static async Task<Message> SendFile(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+		// Обрабатываем голосовое сообщение пользователя
+		static async Task<Message> ReceiveVoiceMessage(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
 		{
+
 			await botClient.SendChatActionAsync(
 				message.Chat.Id,
-				ChatAction.UploadPhoto,
+				ChatAction.Typing,
 				cancellationToken: cancellationToken);
-
-			const string filePath = "Files/tux.png";
-			await using FileStream fileStream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-			var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
-
-			return await botClient.SendPhotoAsync(
-				chatId: message.Chat.Id,
-				photo: new InputFileStream(fileStream, fileName),
-				caption: "Nice Picture",
-				cancellationToken: cancellationToken);
-		}
-		// Пользователь может нажать кнопку для начала взаимодействия с ботом
-		static async Task<Message> StartInlineQuery(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
-		{
-			InlineKeyboardMarkup inlineKeyboard = new(
-				InlineKeyboardButton.WithSwitchInlineQueryCurrentChat("Inline Mode"));
 
 			return await botClient.SendTextMessageAsync(
 				chatId: message.Chat.Id,
-				text: "Press the button to start Inline Query",
-				replyMarkup: inlineKeyboard,
+				text: "Voice message received. Thank you!",
 				cancellationToken: cancellationToken);
 		}
 
-		static async Task<Message> SendVoice(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+		// Отправление голосового сообщения
+		static async Task<Message> SendVoiceMessage(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
 		{
 			await botClient.SendChatActionAsync(
 				message.Chat.Id,
-				ChatAction.RecordVoice,
+				ChatAction.UploadVoice,
 				cancellationToken: cancellationToken);
 
-			const string filePath = "Files/voice_message.ogg";
+			const string filePath = "Files/voice_message.ogg"; // Путь к голосовому сообщению
 			await using FileStream fileStream = new(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
 			var fileName = filePath.Split(Path.DirectorySeparatorChar).Last();
 
