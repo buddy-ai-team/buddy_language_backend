e3ac17f28e1974a8693f992593a3613a7079c9d2
АннаОнис <onischenko.anna11@gmail.com>
2023-10-24 19:38:56 +0300

    Добавлена xml-документация к методам

--- src/BuddyLanguage.Domain/Interfaces/IChatGPTService.cs
+++ src/BuddyLanguage.Domain/Interfaces/IChatGPTService.cs
@@ -8,8 +8,30 @@ namespace BuddyLanguage.Domain.Interfaces
 {
     public interface IChatGPTService
     {
+        /// <summary>
+        /// Получает ответ на сообщение пользователя по заданной теме.
+        /// </summary>
+        /// <param name="userMessage">Сообщение пользователя.</param>
+        /// <param name="userId">Идентификатор пользователя.</param>
+        /// <param name="cancellation">Токен отмены для отмены операции.</param>
+        /// <returns>Ответ на сообщение пользователя.</returns>
         Task<string> GetAnswerOnTopic(string userMessage, Guid userId, CancellationToken cancellation);
+
+        /// <summary>
+        /// Получает ответ на сообщение пользователя.
+        /// </summary>
+        /// <param name="userMessage">Сообщение пользователя.</param>
+        /// <param name="cancellationToken">Токен отмены для отмены операции.</param>
+        /// <returns>Ответ на сообщение пользователя.</returns>
         Task<string> GetAnswer(string userMessage, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Получает ответ на сообщение пользователя, используя заданный промпт.
+        /// </summary>
+        /// <param name="prompt">Промпт для генерации ответа.</param>
+        /// <param name="userMessage">Сообщение пользователя.</param>
+        /// <param name="cancellationToken">Токен отмены для отмены операции.</param>
+        /// <returns>Ответ на сообщение пользователя.</returns>
         Task<string> GetAnswer(string prompt, string userMessage, CancellationToken cancellationToken);
     }
 }
