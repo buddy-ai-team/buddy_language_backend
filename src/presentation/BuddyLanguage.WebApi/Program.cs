41326942f1e4e85b848331f41dd53a40f2bb02d6
IppolitovIS <IppolitovIS@gmail.com>
2023-10-26 22:26:08 +0300

    Создан статический класс для внедрения зависимостей инфраструктуры

--- src/presentation/BuddyLanguage.WebApi/Program.cs
+++ src/presentation/BuddyLanguage.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using BuddyLanguage.ChatGPTServiceLib;
 using BuddyLanguage.Data.EntityFramework;
 using BuddyLanguage.Data.EntityFramework.Repositories;
+using BuddyLanguage.DependencyInjection;
 using BuddyLanguage.Domain.Interfaces;
 using BuddyLanguage.Domain.Services;
 using Microsoft.EntityFrameworkCore;
@@ -14,53 +15,14 @@ using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 //Services
-//Azure TTS
-builder.Services.AddOptions<AzureTTSConfig>()
-    .BindConfiguration("AzureTTSConfig")
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
 
-// Definition of database file name and connection of it as a service
-builder.Services.AddOptions<NpgsqlConnectionStringOptions>()
-    .BindConfiguration("NpgsqlConnectionStringOptions")
-    .ValidateDataAnnotations()
-    .ValidateOnStart();
-
-var config = builder.Configuration
-   .GetSection("NpgsqlConnectionStringOptions")
-   .Get<NpgsqlConnectionStringOptions>();
-
-// Подключение репозитория для работы с Ролями
-builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
-builder.Services.AddScoped<IRoleRepository, RoleRepositoryEf>();
-builder.Services.AddScoped<IUnitOfWork, UnitOfWorkEf>();
-builder.Services.AddScoped<RoleService>();
+builder.Services.AddInfrastructureServices(builder.Configuration);
 
 // Подключение фильтров
 builder.Services.AddControllers(options =>
 {
     options.Filters.Add<CentralizedExceptionHandlingFilter>(order: 1);
 });
-
-builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseNpgsql(config.ConnectionString)
-);
-
-builder.Services.AddChatGptEntityFrameworkIntegration(
-        op => op.UseNpgsql(config.ConnectionString)
-);
-
-builder.Services.AddScoped<IChatGPTService, ChatGPTService>(); 
-
-builder.Services.AddOpenAIService
-    (settings =>
-    {
-        settings.ApiKey = Environment.GetEnvironmentVariable("OPENAI_API_KEY")
-                          ?? throw new InvalidOperationException
-                          ("OPENAI_API_KEY environment variable is not set");
-    });
-
-builder.Services.AddScoped<ISpeechRecognitionService, WhisperSpeechRecognitionService>();
 
 var app = builder.Build();
 
