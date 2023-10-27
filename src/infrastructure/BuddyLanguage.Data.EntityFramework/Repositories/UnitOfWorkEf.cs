82ba79058cb360a7f04db30b2ea7cce55b26a614
YY Ptushko <111455738+ultyyp@users.noreply.github.com>
2023-10-27 18:47:12 +0200

    Added UserService

--- src/infrastructure/BuddyLanguage.Data.EntityFramework/Repositories/UnitOfWorkEf.cs
+++ src/infrastructure/BuddyLanguage.Data.EntityFramework/Repositories/UnitOfWorkEf.cs
@@ -5,17 +5,23 @@ namespace BuddyLanguage.Data.EntityFramework.Repositories;
 public class UnitOfWorkEf : IUnitOfWork
 {
     public IRoleRepository RoleRepository { get; }
+    public IWordEntityRepository WordEntityRepository { get; }
+    public IUserRepository UserRepository { get; }
     
     private readonly AppDbContext _dbContext;
     
     public UnitOfWorkEf(
         AppDbContext dbContext,
-        IRoleRepository roleRepository)
-        {
-            _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
-            RoleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
-        }
-    
+        IRoleRepository roleRepository,
+        IWordEntityRepository wordRepository,
+        IUserRepository userRepository)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        RoleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
+        WordEntityRepository = wordRepository ?? throw new ArgumentNullException(nameof(wordRepository));
+        UserRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+    }
+
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
         => _dbContext.SaveChangesAsync(cancellationToken);
 }
