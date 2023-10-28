b7d21f79a21fd19457d9261f8f75387dc4de14d0
IppolitovIS <IppolitovIS@gmail.com>
2023-10-28 14:30:46 +0300

    Style errors fixed

--- src/infrastructure/BuddyLanguage.Data.EntityFramework/Repositories/UnitOfWorkEf.cs
+++ src/infrastructure/BuddyLanguage.Data.EntityFramework/Repositories/UnitOfWorkEf.cs
@@ -4,10 +4,8 @@ namespace BuddyLanguage.Data.EntityFramework.Repositories;
 
 public class UnitOfWorkEf : IUnitOfWork
 {
-    public IRoleRepository RoleRepository { get; }
-    public IWordEntityRepository WordEntityRepository { get; }
-    
     private readonly AppDbContext _dbContext;
+
     public UnitOfWorkEf(
         AppDbContext dbContext,
         IRoleRepository roleRepository,
@@ -17,6 +15,10 @@ public class UnitOfWorkEf : IUnitOfWork
         RoleRepository = roleRepository ?? throw new ArgumentNullException(nameof(roleRepository));
         WordEntityRepository = wordRepository ?? throw new ArgumentNullException(nameof(wordRepository));
     }
+
+    public IRoleRepository RoleRepository { get; }
+
+    public IWordEntityRepository WordEntityRepository { get; }
 
     public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
         => _dbContext.SaveChangesAsync(cancellationToken);
--- src/infrastructure/BuddyLanguage.Data.EntityFramework/Repositories/WordEntityRepositoryEf.cs
+++ src/infrastructure/BuddyLanguage.Data.EntityFramework/Repositories/WordEntityRepositoryEf.cs
@@ -1,18 +1,13 @@
 ﻿using BuddyLanguage.Domain.Entities;
 using BuddyLanguage.Domain.Interfaces;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Principal;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BuddyLanguage.Data.EntityFramework.Repositories
 {
     public class WordEntityRepositoryEf : EfRepository<WordEntity>, IWordEntityRepository
     {
-        public WordEntityRepositoryEf(AppDbContext dbContext) : base(dbContext)
+        public WordEntityRepositoryEf(AppDbContext dbContext)
+            : base(dbContext)
         {
         }
 
