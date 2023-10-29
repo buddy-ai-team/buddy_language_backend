7cf5fe161e7c9040acfe45e6c34daf78d7a8a1b9
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-10-29 13:59:09 +0400

    Добавлен класс BuddyService

---
+++ src/BuddyLanguage.Domain/Services/BuddyService.cs
@@ -0,0 +1,23 @@
+﻿using BuddyLanguage.Domain.Interfaces;
+
+namespace BuddyLanguage.Domain.Services
+{
+    public class BuddyService
+    {
+        private readonly IChatGPTService _chatGPTService;
+        private readonly ISpeechRecognitionService _speechRecognitionService;
+        private readonly ITextToSpeech _textToSpeechService;
+
+        public BuddyService(
+            IChatGPTService chatGPTService,
+            ISpeechRecognitionService speechRecognitionService,
+            ITextToSpeech textToSpeechService)
+        {
+            _chatGPTService = chatGPTService ?? throw new ArgumentNullException(nameof(chatGPTService));
+            _speechRecognitionService = speechRecognitionService
+                ?? throw new ArgumentNullException(nameof(speechRecognitionService));
+            _textToSpeechService = textToSpeechService
+                ?? throw new ArgumentNullException(nameof(textToSpeechService));
+        }
+    }
+}
