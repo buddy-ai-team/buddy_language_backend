bf8943b5bbfc4e0c2396b7ad449d8005e43365ce
Rodion Mostovoi <rodion.mostovoy@outlook.com>
2023-11-07 23:13:46 +0800

    Add ILanguage interface and improve language support
    Introduced a new ILanguage interface in the BuddyLanguage.Domain.Entities that includes properties such as name, ISO 639-1 language code, and a method to check if a text contains the language. This change will help differentiate and manage multiple languages more effectively.
    
    Other changes include a minor adjustment in UserVoiceCommandHandler in BuddyLanguage.TelegramBot where a newline character was added for better readability of recognized messages.
    
    Furthermore, new package references for LangChain were added in BuddyLanguage.ChatGPTService to improve language recognition and translation services.
    
    Lastly, added a comment in BuddyService.cs to create a custom exception that addresses cases when a user message cannot be recognized.

---
+++ src/BuddyLanguage.Domain/Entities/ILanguage.cs
@@ -0,0 +1,24 @@
+﻿namespace BuddyLanguage.Domain.Entities;
+
+public interface ILanguage : IEntity
+{
+    /// <summary>
+    /// Gets the language name. In native language.
+    /// </summary>
+    string Name { get; init; }
+
+    /// <summary>
+    /// Gets the language name. In English.
+    /// </summary>
+    string NameEn { get; init; }
+
+    /// <summary>
+    /// Gets the ISO 639-1 language code.
+    /// </summary>
+    /// <remarks>
+    /// It should be ValueObject in future.
+    /// </remarks>
+    string Code { get; init; }
+
+    bool TextContainsThisLanguage(string text);
+}
--- src/presentation/BuddyLanguage.TelegramBot/Commands/UserVoiceCommandHandler.cs
+++ src/presentation/BuddyLanguage.TelegramBot/Commands/UserVoiceCommandHandler.cs
@@ -67,7 +67,7 @@ public class UserVoiceCommandHandler : IBotCommandHandler
 
                 await _botClient.SendTextMessageAsync(
                     update.Message.Chat.Id,
-                    $"Recognized: \n```{recognizedMessage}```",
+                    $"Recognized: \n```\n{recognizedMessage}\n```",
                     cancellationToken: cancellationToken);
                 await _botClient.SendTextMessageAsync(
                     update.Message.Chat.Id, answerText, cancellationToken: cancellationToken);
