7841f8176c8cc05c4afffe1bfab5c277bc30701b
YY Ptushko <111455738+ultyyp@users.noreply.github.com>
2023-11-09 19:01:38 +0200

    Added TTS Speed, Migrations

--- src/infrastructure/BuddyLanguage.Data.EntityFramework/Migrations/20231109165007_AddedUserPreferences.Designer.cs
+++ src/infrastructure/BuddyLanguage.Data.EntityFramework/Migrations/20231109165007_AddedUserPreferences.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BuddyLanguage.Data.EntityFramework;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BuddyLanguage.Data.EntityFramework.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231109165007_AddedUserPreferences")]
+    partial class AddedUserPreferences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
