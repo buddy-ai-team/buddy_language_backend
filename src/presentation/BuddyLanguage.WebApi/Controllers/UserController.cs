f8698ccd5a7930f3b86c2c1d4a3a209863a41b00
Rodion Mostovoi <rodion.mostovoy@outlook.com>
2023-11-17 00:20:00 +0800

    Refactor user routes and parameter names for clarity
    Multiple changes were made to improve readability and understandability of the code.
    The GET route 'id' was changed to 'get', and 'telegram-id' was changed to 'get_by_telegram_id',
    now routing and functionality are more self-explanatory. Also, the parameter 'userId' was renamed to 'id' in the GetUserByTelegramId function for consistency.
    Finally, the 'AddWordEntity' function was renamed to 'AddUser' to accurately depict its functionality.

--- src/presentation/BuddyLanguage.WebApi/Controllers/UserController.cs
+++ src/presentation/BuddyLanguage.WebApi/Controllers/UserController.cs
@@ -18,17 +18,23 @@ namespace BuddyLanguage.WebApi.Controllers
             _userService = userService ?? throw new ArgumentNullException(nameof(userService));
         }
 
-        [HttpGet("id")]
+        /*
+         * /user/get?userId=...
+         */
+        [HttpGet("get")]
         public async Task<ActionResult<UserResponse>> GetUserById(Guid userId, CancellationToken cancellationToken)
         {
             var userVar = await _userService.GetUserById(userId, cancellationToken);
             return new UserResponse(userVar.Id, userVar.FirstName, userVar.LastName, userVar.TelegramId);
         }
 
-        [HttpGet("telegram-id")]
-        public async Task<ActionResult<UserResponse>> GetUserByTelegramId(string userId, CancellationToken cancellationToken)
+        /*
+         * /user/get_by_telegram_id?id=...
+         */
+        [HttpGet("get_by_telegram_id")]
+        public async Task<ActionResult<UserResponse>> GetUserByTelegramId(string id, CancellationToken cancellationToken)
         {
-            var userVar = await _userService.GetUserByTelegramId(userId, cancellationToken);
+            var userVar = await _userService.GetUserByTelegramId(id, cancellationToken);
             return new UserResponse(userVar.Id, userVar.FirstName, userVar.LastName, userVar.TelegramId);
         }
 
@@ -45,7 +51,7 @@ namespace BuddyLanguage.WebApi.Controllers
         }
 
         [HttpPost("add")]
-        public async Task<ActionResult<UserResponse>> AddWordEntity(
+        public async Task<ActionResult<UserResponse>> AddUser(
             AddUserRequest request, CancellationToken cancellationToken)
         {
             var userVar = await _userService.AddUser(request.FirstName, request.LastName, request.TelegramId, cancellationToken);
