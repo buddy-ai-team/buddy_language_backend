39feb238e0020661e626980adcc17499ad1b19b3
Дарья <batterfly74@yandex.ru>
2023-11-20 19:18:42 +0500

    #84

---
+++ src/infrastructure/BuddyLanguage.NAudioOggToWavConverter/NAudioOggToWavConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio;
+using NAudio.Vorbis;
+using NAudio.Wave;
+
+namespace BuddyLanguage.NAudioOggToWavConverter
+{
+    internal class NAudioOggToWavConverter
+    {
+        public byte[] ConvertOggToWav(byte[] oggData)
+        {
+            var oggStream = new MemoryStream(oggData);
+            var wavStream = new MemoryStream();
+            var vorbis = new VorbisWaveReader(oggStream);
+            WaveFileWriter.WriteWavFileToStream(wavStream, vorbis);
+            return wavStream.ToArray();
+        }
+    }
+}
