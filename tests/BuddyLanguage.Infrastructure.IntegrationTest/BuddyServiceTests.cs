d08c5377119edeb37a80f1cad1cc4f9c6270b792
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-11-21 22:09:45 +0400

    Изменены тесты

--- tests/BuddyLanguage.Infrastructure.IntegrationTest/BuddyServiceTests.cs
+++ tests/BuddyLanguage.Infrastructure.IntegrationTest/BuddyServiceTests.cs
@@ -100,6 +100,6 @@ public class BuddyServiceTests
         mistakes.WordsCount.Should().Be(1);
         mistakes.Words.Should().NotBeNull();
         mistakes.Words.Should().HaveCount(1);
-        mistakes.Words.Should().Contain("favorite");
+        mistakes.Words.Should().ContainMatch("favorite");
     }
 }
