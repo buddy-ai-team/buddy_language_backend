447a1fe742ea349342a3ad2117c0e7fd0ee95ac3
IppolitovIS <ippolitovIS@yandex.ru>
2023-11-29 21:51:18 +0300

    fix of context builder in test

--- tests/BuddyLanguage.WebAPI.Test/AuthenticationFilterTest.cs
+++ tests/BuddyLanguage.WebAPI.Test/AuthenticationFilterTest.cs
@@ -38,28 +38,4 @@ public class AuthenticationFilterTest
         httpContext.Items["TelegramUserId"].Should().Be("279058397");
         return Task.CompletedTask;
     }
-
-    private AuthorizationFilterContext BuildContext()
-    {
-        var httpContext = new DefaultHttpContext();
-
-        // var json = JsonConvert.SerializeObject(request);
-        // var stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
-        // httpContext.Request.Body = stream;
-        // httpContext.Request.ContentLength = stream.Length;
-        // httpContext.Request.ContentType = "application/json";
-        // ^^^^^^^^
-        // Attach a JSON body
-        httpContext.Request.Headers["Authorization"] =
-            "query_id=AAHdF6IQAAAAAN0XohDhrOrc&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D&auth_date=1662771648&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2";
-
-        var actionDescriptor = new ControllerActionDescriptor
-        {
-            // ActionName = nameof(SomethingController.Post)
-            // ^^^^^^^
-            // Use the endpoint name
-        };
-        var actionContext = new ActionContext(httpContext, new RouteData(), actionDescriptor);
-        return new AuthorizationFilterContext(actionContext, new List<IFilterMetadata>());
-    }
 }
