9b622cf43c617ec7596ab16ed12feceec6b86d90
IppolitovIS <ippolitovIS@yandex.ru>
2023-11-29 22:09:46 +0300

    get user id from header added

--- tests/BuddyLanguage.WebAPI.Test/AuthenticationFilterTest.cs
+++ tests/BuddyLanguage.WebAPI.Test/AuthenticationFilterTest.cs
@@ -1,3 +1,4 @@
+using System.Web;
 using BuddyLanguage.WebApi.Filters;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
@@ -25,7 +26,9 @@ public class AuthenticationFilterTest
         // Arrange
         var httpContext = new DefaultHttpContext();
         httpContext.Request.Headers["Authorization"] =
-            "query_id=AAHdF6IQAAAAAN0XohDhrOrc&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D&auth_date=1662771648&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2";
+            $"tma {GetTestInitData()}";
+
+        var userid = GetTgUserIdFromTma($"tma {GetTestInitData()}");
 
         var actionContext = new ActionContext(httpContext, new RouteData { }, new ControllerActionDescriptor { });
 
@@ -37,5 +40,24 @@ public class AuthenticationFilterTest
         // Assert
         httpContext.Items["TelegramUserId"].Should().Be("279058397");
         return Task.CompletedTask;
+    }
+
+    private string GetTgUserIdFromTma(string tma)
+    {
+        var initDate = tma[4..];
+        var queryParams = HttpUtility.ParseQueryString(initDate);
+
+        string userJson = queryParams["user"]!;
+
+        var (idStartsAtIndex, idEndsAtIndex) = (6, userJson.IndexOf(','));
+        var telegramUserId = userJson[idStartsAtIndex..idEndsAtIndex];
+
+        return telegramUserId;
+    }
+
+    private string GetTestInitData()
+    {
+        return
+            "query_id=AAHdF6IQAAAAAN0XohDhrOrc&user=%7B%22id%22%3A279058397%2C%22first_name%22%3A%22Vladislav%22%2C%22last_name%22%3A%22Kibenko%22%2C%22username%22%3A%22vdkfrost%22%2C%22language_code%22%3A%22ru%22%2C%22is_premium%22%3Atrue%7D&auth_date=1662771648&hash=c501b71e775f74ce10e377dea85a7ea24ecd640b223ea86dfe453e0eaed2e2b2";
     }
 }
