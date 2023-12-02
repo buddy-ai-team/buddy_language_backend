1365c539d9bf743f04d7117fcb38056ea56eb66a
IppolitovIS <ippolitovIS@yandex.ru>
2023-12-02 20:24:08 +0300

    Добавлена валидация запроса!

--- src/presentation/BuddyLanguage.WebApi/Filters/AuthenticationFilter.cs
+++ src/presentation/BuddyLanguage.WebApi/Filters/AuthenticationFilter.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+using System.Text;
 using System.Text.Json;
 using System.Web;
 using BuddyLanguage.WebApi.Filters.AuthenticationData;
@@ -6,8 +8,15 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace BuddyLanguage.WebApi.Filters;
 
+/// <summary>
+/// Фильтр аутентификации.
+/// Проверяет соответствие полученного hash ключа расчётному.
+/// Записывает в поле HttpContext.Items["TelegramUserId"] телеграм ID пользователя,
+/// сделавшего запрос
+/// </summary>
 public class AuthenticationFilter : Attribute, IAuthorizationFilter
 {
+    private const int RequestValidTimeInMinutes = 5;
     private readonly ILogger<AuthenticationFilter> _logger;
 
     public AuthenticationFilter(
@@ -18,6 +27,13 @@ public class AuthenticationFilter : Attribute, IAuthorizationFilter
 
     public void OnAuthorization(AuthorizationFilterContext context)
     {
+        // Получение токен телеграм бота из переменных среды
+        string? botToken = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN")!;
+        if (botToken is null)
+        {
+            throw new ArgumentNullException(nameof(botToken));
+        }
+
         // Получение строки заголовка Authorization
         string initDataHeader = context.HttpContext.Request.Headers["Authorization"].ToString();
 
@@ -29,17 +45,21 @@ public class AuthenticationFilter : Attribute, IAuthorizationFilter
 
         try
         {
-            // Преобразование строки заголовка Authorization к типу данных InitData
-            var queryParams = HttpUtility.ParseQueryString(initDataHeader[4..]);
-            InitData initData = new InitData
+            // Преобразование строки заголовка Authorization к коллекции параметров
+            var listOfParams = ParseHeaderToListOfData(initDataHeader);
+
+            // Получение пользователя из заголовка
+            InitData initData = new InitData()
             {
-                AuthDateRaw = int.Parse(queryParams["auth_date"]!),
-                Hash = queryParams["hash"]!,
-                QueryId = queryParams["query_id"]!,
-                User = JsonSerializer.Deserialize<AuthUser>(queryParams["user"]!)!
+                AuthDateRaw = int.Parse(listOfParams["auth_date"]),
+                Hash = listOfParams["hash"],
+                QueryId = listOfParams["query_id"],
+                User = JsonSerializer.Deserialize<AuthUser>(listOfParams["user"])!
             };
 
-            if (!ValidateInitData(initData))
+            // Проверка соответствия полученного hash расчётному и
+            // запрос "свежий"
+            if (!ValidateInitData(listOfParams, botToken))
             {
                 context.Result = new UnauthorizedResult();
                 return;
@@ -54,10 +74,66 @@ public class AuthenticationFilter : Attribute, IAuthorizationFilter
         }
     }
 
-    private bool ValidateInitData(InitData initData)
+    // Проверить корректность подписи полученных данных
+    private bool ValidateInitData(IDictionary<string, string> pairs, string botToken)
     {
-        // Implement your validation logic here, for example, checking the signature
-        // You may need to convert the logic from your Go code to C#
-        return true; // return true if valid, false otherwise
+        string dataCheckString = BuildDataCheckString(pairs);
+        string signature = Sign(dataCheckString, botToken);
+
+        bool result = signature.Equals(pairs["hash"], StringComparison.OrdinalIgnoreCase);
+        return result;
+    }
+
+    // Создать строку для генерации hash
+    private string BuildDataCheckString(IDictionary<string, string> pairs)
+    {
+        List<string> result = new();
+        foreach (var value in pairs)
+        {
+            if (value.Key != "hash")
+            {
+                result.Add(value.Key + "=" + value.Value);
+            }
+        }
+
+        return string.Join("\n", result);
+    }
+
+    // Сгенерировать hash на основе: кодовой фраз, токена телеграм бота и полученной строки данных
+    private string Sign(string payload, string key)
+    {
+        using (var skHmac = new HMACSHA256(Encoding.UTF8.GetBytes("WebAppData")))
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] skHmacBytes = skHmac.ComputeHash(keyBytes);
+
+            using (var impHmac = new HMACSHA256(skHmacBytes))
+            {
+                byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+                byte[] impHmacBytes = impHmac.ComputeHash(payloadBytes);
+                return BitConverter.ToString(impHmacBytes).Replace("-", string.Empty).ToLower();
+            }
+        }
+    }
+
+    // Проверить "свежесть" запроса
+    private bool IsAuthDateValid(DateTime authDate)
+    {
+        var currentTime = DateTime.UtcNow;
+        return (currentTime - authDate).TotalMinutes <= RequestValidTimeInMinutes;
+    }
+
+    // Преобразовать строку заголовка в коллекцию параметров
+    private IDictionary<string, string> ParseHeaderToListOfData(string header)
+    {
+        var queryParams = HttpUtility.ParseQueryString(header[4..]);
+
+        var listOfParams = new SortedDictionary<string, string>();
+        foreach (var param in queryParams)
+        {
+            listOfParams.Add(param.ToString()!, queryParams[param.ToString()]!);
+        }
+
+        return listOfParams;
     }
 }
