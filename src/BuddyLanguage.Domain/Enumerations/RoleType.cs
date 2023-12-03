d05237d1639b58289aad29e365022e96eec3d20c
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-12-03 16:23:05 +0400

    Добавлен RoleType для роли и реализован метод GetOrCreateRole

---
+++ src/BuddyLanguage.Domain/Enumerations/RoleType.cs
@@ -0,0 +1,20 @@
+﻿namespace BuddyLanguage.Domain.Enumerations
+{
+    public enum RoleType
+    {
+        /// <summary>
+        /// Роль по умолчанию
+        /// </summary>
+        Default,
+
+        /// <summary>
+        /// Общая роль для пользователей
+        /// </summary>
+        Command,
+
+        /// <summary>
+        /// Поьзовательская роль
+        /// </summary>
+        Custom
+    }
+}
