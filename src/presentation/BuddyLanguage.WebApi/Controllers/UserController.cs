7e132fe5ebf37f1410df2062d85fde1510b5902c
АннаОнис <onischenko.anna11@gmail.com>
2023-12-04 22:50:13 +0300

    Возврат User из эндпоинта user/get_by_telegram_id

--- src/presentation/BuddyLanguage.WebApi/Controllers/UserController.cs
+++ src/presentation/BuddyLanguage.WebApi/Controllers/UserController.cs
@@ -32,10 +32,10 @@ namespace BuddyLanguage.WebApi.Controllers
          * /user/get_by_telegram_id?id=...
          */
         [HttpGet("get_by_telegram_id")]
-        public async Task<ActionResult<UserResponse>> GetUserByTelegramId(string id, CancellationToken cancellationToken)
+        public async Task<ActionResult<User>> GetUserByTelegramId(string id, CancellationToken cancellationToken)
         {
             var userVar = await _userService.GetUserByTelegramId(id, cancellationToken);
-            return new UserResponse(userVar.Id, userVar.FirstName, userVar.LastName, userVar.TelegramId);
+            return Ok(userVar);
         }
 
         [HttpPost("update")]
