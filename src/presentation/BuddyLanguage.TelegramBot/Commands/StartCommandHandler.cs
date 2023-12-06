d12363f9f20c2b55d8ab0664a71e4e94f76a3158
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-12-06 10:43:32 +0400

    Добавлено голосовое приветственное  сообщение

--- src/presentation/BuddyLanguage.TelegramBot/Commands/StartCommandHandler.cs
+++ src/presentation/BuddyLanguage.TelegramBot/Commands/StartCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using BuddyLanguage.Domain.Services;
+﻿using BuddyLanguage.Domain.Interfaces;
+using BuddyLanguage.Domain.Services;
 using BuddyLanguage.TelegramBot.Models;
 using Telegram.Bot;
 using Telegram.Bot.Types;
@@ -8,15 +9,18 @@ namespace BuddyLanguage.TelegramBot.Commands;
 public class StartCommandHandler : IBotCommandHandler
 {
     private readonly ITelegramBotClient _botClient;
+    private readonly ITextToSpeech _textToSpeech;
     private readonly UserService _userService;
     private readonly ILogger<StartCommandHandler> _logger;
 
     public StartCommandHandler(
         ITelegramBotClient botClient,
+        ITextToSpeech textToSpeech,
         UserService userService,
         ILogger<StartCommandHandler> logger)
     {
         _botClient = botClient ?? throw new ArgumentNullException(nameof(botClient));
+        _textToSpeech = textToSpeech ?? throw new ArgumentNullException(nameof(textToSpeech));
         _userService = userService ?? throw new ArgumentNullException(nameof(userService));
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
@@ -34,13 +38,25 @@ public class StartCommandHandler : IBotCommandHandler
             info.FirstName,
             info.LastName);
 
-        await _userService.TryRegister(
+        var user = await _userService.TryRegister(
             info.FirstName, info.LastName, info.UserId, cancellationToken);
+        var nativeLanguage = user.UserPreferences.NativeLanguage;
+        var voice = user.UserPreferences.SelectedVoice;
+        var speed = user.UserPreferences.SelectedSpeed;
 
         const string welcomeMessage =
             "Привет! Поздравляю вас с регистрацией! Расскажу немного о себе, я ваш бот-собеседник. Вы можете отправлять голосовые сообщения на английском или русском языке не более 3х минут и я вам отвечу. Может поговорить на интересующие вас темы. Также я могу проводить грамматический анализ сообщений и исправлять ошибки.";
         await _botClient.SendTextMessageAsync(
             info.ChatId, welcomeMessage, cancellationToken: cancellationToken);
+
+        var welcomeMessageBytes = await _textToSpeech.TextToWavByteArrayAsync(
+            welcomeMessage, nativeLanguage, voice, speed, cancellationToken);
+
+        using var memoryStreamAnswer = new MemoryStream(welcomeMessageBytes);
+        await _botClient.SendVoiceAsync(
+            chatId: update.Message!.Chat.Id,
+            voice: InputFile.FromStream(memoryStreamAnswer, "welcomMessage.ogg"),
+            cancellationToken: cancellationToken);
 
         await _botClient.SendTextMessageAsync(
             info.ChatId,
