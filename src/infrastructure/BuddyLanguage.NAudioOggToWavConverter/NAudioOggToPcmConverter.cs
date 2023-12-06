f020c8870a2260ea4ffbfa348122a9c08aab7cd9
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-12-06 20:37:29 +0400

    Изменен конвертер

--- src/infrastructure/BuddyLanguage.NAudioOggToWavConverter/NAudioOggToPcmConverter.cs
+++ src/infrastructure/BuddyLanguage.NAudioOggToWavConverter/NAudioOggToPcmConverter.cs
@@ -20,20 +20,31 @@ namespace BuddyLanguage.NAudioOggToWavConverter
     {
         public byte[] ConvertOggToPcm(byte[] oggData)
         {
-            using (var oggStream = new MemoryStream(oggData))
-            using (var pcmStream = new MemoryStream())
-            {   
-                using (var readerStream = new VorbisWaveReader(oggStream))
-                {
-                    // 16kHz, 16bit, mono
-                    var resampler = new WdlResamplingSampleProvider(readerStream.ToSampleProvider(), 16000);
-                    var to16Bit = new WaveFloatTo16Provider(resampler.ToWaveProvider());
-                    var mono = new StereoToMonoProvider16(to16Bit);
+            using var oggStream = new MemoryStream(oggData);
+            using var vorbis = new VorbisWaveReader(oggStream);
+            var resampler = new WdlResamplingSampleProvider(vorbis, 16000);
 
-                    WaveFileWriter.WriteWavFileToStream(pcmStream, mono);
-                    return pcmStream.ToArray();
+            // Converts from float to 16-bit PCM
+            var to16Bit = new WaveFloatTo16Provider(resampler.ToWaveProvider());
+
+            // Converts to mono
+            var mono = new StereoToMonoProvider16(to16Bit) { LeftVolume = 0.5f, RightVolume = 0.5f };
+
+            // Create a new MemoryStream to store the converted PCM data
+            using var pcmStream = new MemoryStream();
+
+            // Write the converted PCM data to the MemoryStream
+            using (var pcmWriter = new WaveFileWriter(pcmStream, mono.WaveFormat))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+                while ((bytesRead = mono.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    pcmWriter.Write(buffer, 0, bytesRead);
                 }
             }
+
+            return pcmStream.ToArray();
         }
     }
 }
